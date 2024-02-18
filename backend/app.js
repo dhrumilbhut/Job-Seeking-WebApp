e0832050e15a7009f1034a30c5c6d6f9aca572eb
Dhrumil <dhrumilbhut@gmail.com>
2024-02-18 08:46:37 +0530

    fix: rename main file

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,15 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
+app.use("/", (req, res) => {
+  res.send("Welcome to Job Portal API");
+});
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
 dbConnection();
 
 app.use(errorMiddleware);
