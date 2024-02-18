7e0f32d9bd689caf36a9612cf22d73b3785b633d
Dhrumil <dhrumilbhut@gmail.com>
2024-02-18 09:16:14 +0530

    fix: removed httpOnly

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,8 +3,7 @@ export const sendToken = (user, statusCode, res, message) => {
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // Set httpOnly to true
+    )
   };
 
   res.status(statusCode).cookie("token", token, options).json({
