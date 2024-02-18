7580a1fccdbbf7d228ba31e2cf7876d72674933b
Dhrumil <dhrumilbhut@gmail.com>
2024-02-18 09:26:59 +0530

    fix:

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,7 +3,8 @@ export const sendToken = (user, statusCode, res, message) => {
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    )
+    ),
+    httpOnly: true, // Set httpOnly to true
   };
 
   res.status(statusCode).cookie("token", token, options).json({
